04be3a8aeb80bb945a1c38da6ea8dcf16db36ecd
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-08-28 19:47:12 -0300

    Create teste3.py

---
+++ teste3.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um numero '))
+n2 = int(input('Digite outro numero '))
+soma = n1+n2
+sub = n1-n2
+mult = n1*n2
+div = n1/n2
+print('resultado é ', soma)
+print('resultado é ', sub)
+print('resultado é ', mult)
+print('resultado é ', div)
